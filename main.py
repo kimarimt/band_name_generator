b2fbedc5773835232b0634c53faadc148bf58f48
Alec German <kimarimt@protonmail.com>
2025-03-06 07:52:22 -0500

    fix formatting

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 def main():
-  print('Welcome to the Band Name Generator')
-  city = input('What\'s the name of the city you grew up in?\n')
-  pet_name = input('What\'s your pet\'s name?\n')
-  print(f'Your band name could {city} {pet_name}')
+    print('Welcome to the Band Name Generator')
+    city = input('What\'s the name of the city you grew up in?\n')
+    pet_name = input('What\'s your pet\'s name?\n')
+    print(f'Your band name could {city} {pet_name}')
 
 
 if __name__ == '__main__':
-  main()
+    main()
