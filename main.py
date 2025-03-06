1939ace099ef22fe85ee788cbdd987f380da24ff
Alec German <kimarimt@protonmail.com>
2025-03-06 04:31:59 -0500

    finished day 1

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    print('Hello, World')
+    print('Welcome to the Band Name Generator')
+    city = input('What\'s the name of the city you grew up in?\n')
+    pet_name = input('What\'s your pet\'s name?\n')
+    print(f'Your band name could {city} {pet_name}')
 
 
 if __name__ == '__main__':
